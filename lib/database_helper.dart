da0c8ada8d01321317405c81678a37d35ed4acb0
Satriahrl_ <satriaherlambang22@gmail.com>
2025-12-13 00:35:55 +0700

    feat: Mengimplementasikan singleton DatabaseHelper dengan operasi CRUD.

---
+++ lib/database_helper.dart
@@ -0,0 +1,49 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'note_model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('notes.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE notes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT,
+      content TEXT)  
+    ''');
+  }
+
+  Future<int> create(Note note) async {
+    final db = await instance.database;
+    return await db.insert('notes', note.toMap());
+  }
+
+  Future<List<Note>> readAllNotes() async {
+    final db = await instance.database;
+    final result = await db.query('notes');
+
+    return result.map((json) => Note.fromMap(json)).toList();
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+    return await db.delete('notes', where: 'id = ?', whereArgs: [id]);
+  }
+}
