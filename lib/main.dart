60f15c3039592c6d48bc5cdac4f7d2c2a12ed1af
Satriahrl_ <satriaherlambang22@gmail.com>
2025-12-13 01:53:58 +0700

    feat: Membangun UI untuk daftar catatan, membuat formulir, dan aksi penghapusan.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,121 @@
+import 'package:flutter/material.dart';
+import 'database_helper.dart';
+import 'note_model.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(home: MySimpleNotes(), debugShowCheckedModeBanner: false),
+  );
+}
+
+class MySimpleNotes extends StatefulWidget {
+  const MySimpleNotes({super.key});
+
+  @override
+  State<MySimpleNotes> createState() => _MySimpleNotesState();
+}
+
+class _MySimpleNotesState extends State<MySimpleNotes> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
+  void _refreshNotes() {
+    setState(() {});
+  }
+
+  void _showForm() {
+    _titleController.clear();
+    _contentController.clear();
+
+    showModalBottomSheet(
+      context: context,
+      elevation: 5,
+      isScrollControlled: true,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          top: 15,
+          left: 15,
+          right: 15,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: const InputDecoration(hintText: 'Judul Catatan'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _contentController,
+              decoration: const InputDecoration(hintText: 'Isi Catatan'),
+              maxLines: 3,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                String title = _titleController.text;
+                String content = _contentController.text;
+
+                if (title.isNotEmpty && content.isNotEmpty) {
+                  await DatabaseHelper.instance.create(
+                    Note(title: title, content: content),
+                  );
+
+                  Navigator.of(context).pop();
+                  _refreshNotes();
+                }
+              },
+              child: const Text('Simpan Catatan'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Simple Notes SQL')),
+      body: FutureBuilder<List<Note>>(
+        future: DatabaseHelper.instance.readAllNotes(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.data!.isEmpty) {
+            return const Center(child: Text('Belum ada catatan'));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              Note note = snapshot.data![index];
+              return Card(
+                child: ListTile(
+                  title: Text(note.title),
+                  subtitle: Text(note.content),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () async {
+                      await DatabaseHelper.instance.delete(note.id!);
+                      _refreshNotes();
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: _showForm,
+      ),
+    );
+  }
+}
